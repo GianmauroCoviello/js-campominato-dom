18db18498dc4bafb92edd67d071d475ca77a2009
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-05-30 16:29:03 +0200

    [DOCS] Modifiche aggiuntive.

--- js/script.js
+++ js/script.js
@@ -15,10 +15,16 @@ function randomBombs(min, max) {
     
 }
 
+function punteggioSquare(x, y) {
+
+    let somma = x - y
+    return somma
+    
+}
+
+   
 
 
-
-console.log(createSquare())
 // VARIABILE CHE INSERISCA LA GRIGLIA ALL' INTERNO DEL DOM
 let button = document.getElementById('riproduci')
 
@@ -54,42 +60,65 @@ button.addEventListener('click', function() {
     for (i = 0; i < 16 ; i++) {
         let numberBombs = randomBombs(1, numberSquare)
         bombs.push(numberBombs)
+
+        
         
         
     }
     console.log(bombs)
-   
+    
+
+    
     // CICLO FOR INSERITO PER CICLARE I QUADRATINI
     for (i =0; i < numberSquare ; i++) {
         // VARIABILE CHE RIPRENDE LA FUNZIONE 
         let square = createSquare(bombs)
 
+        
+   
+
         // VARIABILE CON UNA PROPRIETA CHE DEFINISCE LA RADICE QUADRATA DEL NUMERO DI RIFERIMENTO
         let radiceSquare = Math.sqrt(numberSquare)
+
 
         square.style.width =  `calc(100% / ${radiceSquare}) `
         square.style.height = `calc(100% / ${radiceSquare}) `
         // VARIABILE CHE INNIETTA GLI INDICI NUMERICI DEL CICLO ALL'INTERNO DEI QUADRATINI
         square.innerText = i + 1
 
+        let result = (punteggioSquare(numberSquare, 16))
+        console.log(result)
+
         // ADDEVENTLISTENER ALLA CLASSE SQUARE PER FAR CAMBIARE E RIMUOVERE IL COLORE AI QUADRATINI
         square.addEventListener('click', function(){
             // FUNZIONI CHE AGGIUNGONO E TOLGONO LA CLASSE PER IL COLORE AI QUADRATINI('SQUARE') 
-
+    
             // console.log per riuscire a dare il numero esatto del quadratino in cui ci troviamo
             // console.log(this.innerText)
             if (!bombs.includes(parseInt(this.innerText))) {
                 this.classList.add('click-color')
                 punteggio++
-                
-                
-                
+
+            }else if (punteggio === result) {
+                // alert( `HAI VINTO, HAI TOTALIZZATO UN PUNTEGGIO PARI A: ${punteggio} `)
+                alert('HAI VINTO')
+                console.log(result)
             } else{
                 this.classList.add('click-bombs')
                 alert( `OPSS.... HAI PERSO, HAI TOTALIZZATO UN PUNTEGGIO PARI A: ${punteggio} `)
-
+    
             }
             console.log(punteggio) 
+
+                
+                
+                   
+            // } else{
+            //     this.classList.add('click-bombs')
+            //     alert( `OPSS.... HAI PERSO, HAI TOTALIZZATO UN PUNTEGGIO PARI A: ${punteggio} `)
+
+            // }
+            // console.log(punteggio) 
             
         })
 
